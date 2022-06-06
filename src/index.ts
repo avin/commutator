52a1d4cd350f055973cdb8e1e9aa46f298984bdb
a.vinogradov <a.vinogradov@bpcbt.com>
2022-06-06 17:41:14 +0300

    reformat

--- src/index.ts
+++ src/index.ts
@@ -1,91 +1,92 @@
 import mitt, { Emitter } from 'mitt';
 
 type CommutatorOptions = {
-    serviceId: string;
-    target: Window;
+  serviceId: string;
+  target: Window;
+  origin?: string;
 };
 
 type ReqMessageObject = {
-    type: 'req';
-    funcName: string;
-    params: any;
-    id: string;
+  type: 'req';
+  funcName: string;
+  params: any;
+  id: string;
 };
 
 type ResMessageObject = {
-    type: 'res';
-    result: any;
-    id: string;
+  type: 'res';
+  result: any;
+  id: string;
 };
 
 export class Commutator {
-    options!: CommutatorOptions;
+  options!: CommutatorOptions;
 
-    private emitter!: Emitter<any>;
+  private emitter!: Emitter<any>;
 
-    constructor(options: CommutatorOptions) {
-        this.options = options;
-        this.emitter = mitt();
+  constructor(options: CommutatorOptions) {
+    this.options = options;
+    this.emitter = mitt();
 
-        window.addEventListener('message', this.handleMessage.bind(this), false);
+    window.addEventListener('message', this.handleMessage.bind(this), false);
+  }
+
+  private handleMessage(event: MessageEvent) {
+    let data: string = event.data;
+    if (data?.startsWith && data?.startsWith(`${this.options.serviceId}::`)) {
+      data = data.replace(`${this.options.serviceId}::`, '');
+      const dataObj = JSON.parse(data) as ReqMessageObject | ResMessageObject;
+
+      if (dataObj.type === 'res') {
+        this.emitter.emit(`res_${dataObj.id}`, dataObj);
+      } else if (dataObj.type === 'req') {
+        this.emitter.emit(`req_${dataObj.funcName}`, dataObj);
+      }
     }
+  }
 
-    private handleMessage(event: MessageEvent) {
-        let data: string = event.data;
-        if (data?.startsWith && data?.startsWith(`${this.options.serviceId}::`)) {
-            data = data.replace(`${this.options.serviceId}::`, '');
-            const dataObj = JSON.parse(data) as ReqMessageObject | ResMessageObject;
+  call<T>(funcName: string, params: any): Promise<T> {
+    const messageId = Commutator.makeId();
+    const formattedMessage = `${this.options.serviceId}::${JSON.stringify({
+      funcName,
+      params,
+      id: messageId,
+      type: 'req',
+    })}`;
 
-            if (dataObj.type === 'res') {
-                this.emitter.emit(`res_${dataObj.id}`, dataObj);
-            } else if (dataObj.type === 'req') {
-                this.emitter.emit(`req_${dataObj.funcName}`, dataObj);
-            }
-        }
-    }
+    this.options.target.postMessage(formattedMessage, this.options.origin || '*');
 
-    call<T>(funcName: string, params: any): Promise<T> {
-        const messageId = Commutator.makeId();
+    return new Promise((resolve, reject) => {
+      this.emitter.on(`res_${messageId}`, (dataObj: ResMessageObject) => {
+        this.emitter.off(`res_${messageId}`);
+        return resolve(dataObj.result as T);
+      });
+    });
+  }
+
+  expose(funcName: string, cb: (params: any) => any) {
+    this.emitter.on(`req_${funcName}`, (dataObj: ReqMessageObject) => {
+      void (async () => {
+        const result = await cb(dataObj.params);
+
         const formattedMessage = `${this.options.serviceId}::${JSON.stringify({
-            funcName,
-            params,
-            id: messageId,
-            type: 'req',
+          result,
+          id: dataObj.id,
+          type: 'res',
         })}`;
 
-        this.options.target.postMessage(formattedMessage);
+        this.options.target.postMessage(formattedMessage, this.options.origin || '*');
+      })();
+    });
+  }
 
-        return new Promise((resolve, reject) => {
-            this.emitter.on(`res_${messageId}`, (dataObj: ResMessageObject) => {
-                this.emitter.off(`res_${messageId}`);
-                return resolve(dataObj.result as T);
-            });
-        });
+  static makeId(length = 10) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < length; i += 1) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-
-    expose(funcName: string, cb: (params: any) => any) {
-        this.emitter.on(`req_${funcName}`, (dataObj: ReqMessageObject) => {
-            void (async () => {
-                const result = await cb(dataObj.params);
-
-                const formattedMessage = `${this.options.serviceId}::${JSON.stringify({
-                    result,
-                    id: dataObj.id,
-                    type: 'res',
-                })}`;
-
-                this.options.target.postMessage(formattedMessage);
-            })();
-        });
-    }
-
-    static makeId(length = 10) {
-        let result = '';
-        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const charactersLength = characters.length;
-        for (let i = 0; i < length; i += 1) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    }
+    return result;
+  }
 }
